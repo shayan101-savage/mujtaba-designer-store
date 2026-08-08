7835995bd96d2009024be6121630bcd48f421596
shayan101-savage <shaynkashif101@gmail.com>
2026-08-08 16:10:40 +0000

    oj

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,4 @@
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-const api = require('./api.cjs');
+import api from './api.cjs';
 
 export const handler = api.handler;
 export default handler;
